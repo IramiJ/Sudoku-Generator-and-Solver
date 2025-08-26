a33156040bd9c2cd6230c112674bd88b9cd86e70
Colliname <yugiohduel3@gmail.com>
2025-08-26 11:36:18 +0200

    you can add numbers to the cells through buttons, cell is a class now

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import sys
 from PyQt5.QtWidgets import (QApplication, QMainWindow, QLabel, QWidget, QLineEdit, QGridLayout, QPushButton)
-
+from PyQt5.QtCore import Qt
 class MainWindow(QMainWindow):
  def __init__(self):
   super().__init__()
+  self.selected_number = None
   self.setWindowTitle("Sudoku")
   self.setGeometry(0,0,450,450)
   self.setMaximumHeight(450)
@@ -13,29 +14,43 @@ class MainWindow(QMainWindow):
  def initUI(self):
   central_widget = QWidget()
   self.setCentralWidget(central_widget)
-
+  a = 2
+  for n in range(1, 10):
+    btn = QPushButton(str(n))
+    btn.clicked.connect(lambda _, num=n: self.select_number(num))
+    self.statusBar().addPermanentWidget(btn)
   cells = []
   grid = QGridLayout()
   for row in range(9):
    row_cells = []
    for col in range(9):
-    cell = QLineEdit()
-    cell.setFixedSize(50,50)
-    cell.setMaxLength(1)
-    grid.addWidget(cell, row, col)
+    class cell(QLineEdit):
+     def __init__(self, mainwindow):
+      super().__init__()
+      self.mainwindow = mainwindow
+      self.setFixedSize(50,50)
+      self.setMaxLength(1)
+      self.setAlignment(Qt.AlignHCenter)
+      self.setReadOnly(True)
+     def focusInEvent(self, event):
+      self.setStyleSheet("background-color: yellow;")
+      if self.mainwindow.selected_number is not None:
+        self.setText(self.mainwindow.selected_number)
+      super().focusInEvent(event)
+     def focusOutEvent(self, event):
+      self.setStyleSheet("")
+      super().focusOutEvent(event)
+    c = cell(self)
+    grid.addWidget(c, row, col)
     row_cells.append(cells)
   cells.append(row_cells)
   central_widget.setLayout(grid)
-  self.selected_number = None
-  for n in range(1, 10):
-    btn = QPushButton(str(n))
-    btn.clicked.connect(lambda _, num=n: self.select_number(num))
-    self.statusBar().addPermanentWidget(btn)
+  
   grid.setSpacing(0)
   grid.setContentsMargins(0,0,0,0)
 
  def select_number(self, num):
-  self.selected_num = num
+  self.selected_number = str(num)
   self.statusBar().showMessage(f"Selected number: {num}", 0)
 
 
